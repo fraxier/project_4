5aac26e29628d4d408b59cb1815010cf388ccdd9
Francis <fraxier95@outlook.com>
2022-11-02 11:31:20 +1100

    some cleanup and carousel styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require popper
+//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "controllers"
